d6808a2e102636e9f44191a96b00fb98b37c4879
bohdan-ly <bohdan.lyshchenko@gmail.com>
2024-07-30 16:19:07 +0300

    wip

--- routes/realtyRoutes.js
+++ routes/realtyRoutes.js
@@ -59,8 +59,8 @@ router.post(
 );
 
 router.post("/wayforpay", function (req, res) {
-  console.log("Wayforpay", req);
-  console.log("BODY!!!", req.body);
+  console.log("Wayforpay", req.req);
+  // console.log("BODY!!!", req.body);
 
   res.status(200).json({
     status: "success",
